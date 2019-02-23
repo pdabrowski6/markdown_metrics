1861e48db8d4dfbaf4ff89cd04c51bc58e79b6fc
Paweł Dąbrowski <dziamber@gmail.com>
2019-02-23 14:53:30 +0100

    Update specs for paragraph

--- spec/lib/markdown_metrics/elements/inline/paragraph_spec.rb
+++ spec/lib/markdown_metrics/elements/inline/paragraph_spec.rb
@@ -3,16 +3,17 @@ require 'spec_helper'
 RSpec.describe MarkdownMetrics::Elements::Inline::Paragraph do
   let(:line) { "Lorem ipsum" }
   let(:next_line) { nil }
+  let(:element) { described_class.new(lines: [line, next_line], start_at: 0) }
 
   describe '.match_element' do
     it { expect(described_class.match_element(line, next_line)).not_to be_nil }
   end
 
-  describe '.element_name' do
-    it { expect(described_class.element_name).to eq(:paragraph) }
+  describe '#name' do
+    it { expect(element.name).to eq(:paragraph) }
   end
 
-  describe '.value' do
-    it { expect(described_class.value(line)).to eq('Lorem ipsum') }
+  describe '#value' do
+    it { expect(element.value).to eq('Lorem ipsum') }
   end
 end
