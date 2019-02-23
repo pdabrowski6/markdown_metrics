3dd69cb6619801640b206e0d12b4c212ba5eab01
Paweł Dąbrowski <dziamber@gmail.com>
2019-02-23 14:53:02 +0100

    Update specs for h2

--- spec/lib/markdown_metrics/elements/inline/h2_spec.rb
+++ spec/lib/markdown_metrics/elements/inline/h2_spec.rb
@@ -3,16 +3,17 @@ require 'spec_helper'
 RSpec.describe MarkdownMetrics::Elements::Inline::H2 do
   let(:line) { "## header" }
   let(:next_line) { nil }
+  let(:element) { described_class.new(lines: [line, next_line], start_at: 0) }
 
   describe '.match_element' do
     it { expect(described_class.match_element(line, next_line)).not_to be_nil }
   end
 
-  describe '.element_name' do
-    it { expect(described_class.element_name).to eq(:h2) }
+  describe '#name' do
+    it { expect(element.name).to eq(:h2) }
   end
 
-  describe '.value' do
-    it { expect(described_class.value(line)).to eq('header') }
+  describe '#value' do
+    it { expect(element.value).to eq('header') }
   end
 end
