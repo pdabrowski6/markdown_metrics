e3b17d350b4ac2e95e6ff4d9eedcd03e812c74bb
Paweł Dąbrowski <dziamber@gmail.com>
2019-02-23 15:36:07 +0100

    Update specs for block pre

--- spec/lib/markdown_metrics/elements/block/pre_spec.rb
+++ spec/lib/markdown_metrics/elements/block/pre_spec.rb
@@ -3,12 +3,53 @@ require 'spec_helper'
 RSpec.describe MarkdownMetrics::Elements::Block::Pre do
   let(:line) { '```ruby' }
   let(:next_line) { 'class Some' }
+  let(:element) { described_class.new(lines: [line, next_line], start_at: 0) }
 
   describe '.match_element' do
     it { expect(described_class.match_element(line, next_line)).not_to be_nil }
   end
 
-  describe '.element_name' do
-    it { expect(described_class.element_name).to eq(:pre) }
+  describe '#name' do
+    it { expect(element.name).to eq(:pre) }
+  end
+
+  describe '#value' do
+    let(:lines) do
+      [
+        '```ruby',
+        'class Something',
+        'end',
+        '```'
+      ]
+    end
+
+    let(:element) { described_class.new(lines: lines, start_at: 0) }
+
+    it 'returns value' do
+      expect(element.value).to eq("class Something\nend")
+    end
+
+    it 'sets the number of lines to skip' do
+      element.value
+
+      expect(element.skip_lines_until).to eq(3)
+    end
+  end
+
+  describe '#attributes' do
+    let(:lines) do
+      [
+        '```ruby',
+        'class Something',
+        'end',
+        '```'
+      ]
+    end
+
+    let(:element) { described_class.new(lines: lines, start_at: 0) }
+
+    it 'returns language name' do
+      expect(element.attributes).to eq({ language: 'ruby' })
+    end
   end
 end
