e1c286e450234b5df8b99f468d8c4a3de554f4c8
Paweł Dąbrowski <dziamber@gmail.com>
2019-02-23 14:53:18 +0100

    Update specs for image

--- spec/lib/markdown_metrics/elements/inline/image_spec.rb
+++ spec/lib/markdown_metrics/elements/inline/image_spec.rb
@@ -3,17 +3,18 @@ require 'spec_helper'
 RSpec.describe MarkdownMetrics::Elements::Inline::Image do
   let(:line) { "![image_desc](http://url.com/image)" }
   let(:next_line) { nil }
+  let(:element) { described_class.new(lines: [line, next_line], start_at: 0) }
 
   describe '.match_element' do
     it { expect(described_class.match_element(line, next_line)).not_to be_nil }
   end
 
-  describe '.element_name' do
-    it { expect(described_class.element_name).to eq(:image) }
+  describe '#name' do
+    it { expect(element.name).to eq(:image) }
   end
 
-  describe '.value' do
-    it { expect(described_class.value(line)).to eq('http://url.com/image') }
+  describe '#value' do
+    it { expect(element.value).to eq('http://url.com/image') }
   end
 
   describe '.attributes' do
