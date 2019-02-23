96a0f0c99d172774a28fd4199fc593081a75cadf
Paweł Dąbrowski <dziamber@gmail.com>
2019-02-23 15:36:16 +0100

    Update specs for block table

--- spec/lib/markdown_metrics/elements/block/table_spec.rb
+++ spec/lib/markdown_metrics/elements/block/table_spec.rb
@@ -3,12 +3,42 @@ require 'spec_helper'
 RSpec.describe MarkdownMetrics::Elements::Block::Table do
   let(:line) { 'Header | Header' }
   let(:next_line) { '------ | -----' }
+  let(:element) { described_class.new(lines: [line, next_line], start_at: 0) }
 
   describe '.match_element' do
     it { expect(described_class.match_element(line, next_line)).not_to be_nil }
   end
 
-  describe '.element_name' do
-    it { expect(described_class.element_name).to eq(:table) }
+  describe '#name' do
+    it { expect(element.name).to eq(:table) }
+  end
+
+  describe '#value' do
+    let(:lines) do
+      [
+        'Header | Header',
+        '------ | -----',
+        'value | value1',
+        'value2 | value3'
+      ]
+    end
+
+    let(:element) { described_class.new(lines: lines, start_at: 0) }
+
+    it 'returns table values' do
+      expect(element.value).to eq({
+        headers: ['Header ', ' Header'],
+        rows: [
+          ['value ', ' value1'],
+          ['value2 ', ' value3']
+        ]
+      })
+    end
+
+    it 'sets the number of lines to skip' do
+      element.value
+
+      expect(element.skip_lines_until).to eq(3)
+    end
   end
 end
