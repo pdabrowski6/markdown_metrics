c466677c4298db15ac7c487037e086c4b5207658
Paweł Dąbrowski <dziamber@gmail.com>
2019-02-23 14:18:42 +0100

    Load quote block element

--- lib/markdown_metrics.rb
+++ lib/markdown_metrics.rb
@@ -15,6 +15,7 @@ require 'markdown_metrics/elements/inline/empty_line'
 require 'markdown_metrics/elements/inline/paragraph'
 require 'markdown_metrics/elements/block/table'
 require 'markdown_metrics/elements/block/pre'
+require 'markdown_metrics/elements/block/quote'
 
 require 'markdown_metrics/file_lines'
 require 'markdown_metrics/line_top_element'
