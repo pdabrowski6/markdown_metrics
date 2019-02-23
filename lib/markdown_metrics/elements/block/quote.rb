c6e0db43424aa1af69d2669a36353b0e9de43ed2
Paweł Dąbrowski <dziamber@gmail.com>
2019-02-23 15:35:52 +0100

    Add value for block quote

--- lib/markdown_metrics/elements/block/quote.rb
+++ lib/markdown_metrics/elements/block/quote.rb
@@ -3,13 +3,33 @@
 module MarkdownMetrics
   module Elements
     module Block
-      class Quote
+      class Quote < MarkdownMetrics::Elements::Base
         def self.match_element(line, next_line)
           line.to_s.match(/^\>/)
         end
 
-        def self.element_name
+        def name
           :quote
+        end
+
+        def value
+          end_line = nil
+          next_line = @start_at
+
+          @lines[(@start_at + 1)..-1].each do |code_line|
+            next_line += 1
+            unless code_line.match(/^\>/)
+              end_line = next_line - 1
+              break
+            end
+
+            end_line = @lines.size - 1 if end_line.nil?
+          end
+
+          if !end_line.nil?
+            @skip_lines_until = end_line
+            @lines[@start_at..end_line].map { |e| e[1..-1] }
+          end
         end
       end
     end
