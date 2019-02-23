df6e3b9995cab273a47167208123d846b0a72ac7
Paweł Dąbrowski <dziamber@gmail.com>
2019-02-23 14:07:54 +0100

    Update h6 element specs

--- spec/lib/markdown_metrics/elements/inline/h6_spec.rb
+++ spec/lib/markdown_metrics/elements/inline/h6_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe MarkdownMetrics::Elements::Inline::H6 do
   let(:line) { "###### header" }
   let(:next_line) { nil }
 
-  describe '.match' do
-    it { expect(described_class.match(line, next_line)).not_to be_nil }
+  describe '.match_element' do
+    it { expect(described_class.match_element(line, next_line)).not_to be_nil }
   end
 
   describe '.element_name' do
