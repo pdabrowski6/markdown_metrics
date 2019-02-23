286b128118b50e07708d586b6c6f1f7da93530a6
Paweł Dąbrowski <dziamber@gmail.com>
2019-02-23 15:36:03 +0100

    Update specs for block list

--- spec/lib/markdown_metrics/elements/block/list_spec.rb
+++ spec/lib/markdown_metrics/elements/block/list_spec.rb
@@ -1,6 +1,16 @@
 require 'spec_helper'
 
 RSpec.describe MarkdownMetrics::Elements::Block::List do
+  let(:lines) do
+    [
+      '* First item',
+      '* Second item',
+      '* Third item',
+    ]
+  end
+
+  let(:element) { described_class.new(lines: lines, start_at: 0) }
+
   describe '.match_element' do
     [
       ['* First item', '* Second element'],
@@ -11,7 +21,39 @@ RSpec.describe MarkdownMetrics::Elements::Block::List do
     end
   end
 
-  describe '.element_name' do
-    it { expect(described_class.element_name).to eq(:list) }
+  describe '#name' do
+    it { expect(element.name).to eq(:list) }
+  end
+
+  describe '#value' do
+    it 'returns value' do
+      expect(element.value).to eq([' First item', ' Second item', ' Third item'])
+    end
+
+    it 'sets the number of lines to skip' do
+      element.value
+
+      expect(element.skip_lines_until).to eq(2)
+    end
+  end
+
+  describe '#attributes' do
+    it 'returns dash type' do
+      element = described_class.new(lines: ['- item'], start_at: 0)
+
+      expect(element.attributes).to eq({ type: :dash })
+    end
+
+    it 'returns star type' do
+      element = described_class.new(lines: ['* item'], start_at: 0)
+
+      expect(element.attributes).to eq({ type: :star })
+    end
+
+    it 'returns number type' do
+      element = described_class.new(lines: ['1. item'], start_at: 0)
+
+      expect(element.attributes).to eq({ type: :number })
+    end
   end
 end
