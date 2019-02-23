3c40d6ad0e5d33895b94cb15097668a047f201fd
Paweł Dąbrowski <dziamber@gmail.com>
2019-02-23 15:36:12 +0100

    Update specs for block quote

--- spec/lib/markdown_metrics/elements/block/quote_spec.rb
+++ spec/lib/markdown_metrics/elements/block/quote_spec.rb
@@ -3,12 +3,35 @@ require 'spec_helper'
 RSpec.describe MarkdownMetrics::Elements::Block::Quote do
   let(:line) { '> Some quote' }
   let(:next_line) { '> Author' }
+  let(:element) { described_class.new(lines: [line, next_line], start_at: 0) }
 
   describe '.match_element' do
     it { expect(described_class.match_element(line, next_line)).not_to be_nil }
   end
 
-  describe '.element_name' do
-    it { expect(described_class.element_name).to eq(:quote) }
+  describe '#name' do
+    it { expect(element.name).to eq(:quote) }
+  end
+
+  describe '#value' do
+    let(:lines) do
+      [
+        '>Some quote',
+        '>',
+        '>Author',
+      ]
+    end
+
+    let(:element) { described_class.new(lines: lines, start_at: 0) }
+
+    it 'returns value' do
+      expect(element.value).to eq(['Some quote', '', 'Author'])
+    end
+
+    it 'sets the number of lines to skip' do
+      element.value
+
+      expect(element.skip_lines_until).to eq(2)
+    end
   end
 end
