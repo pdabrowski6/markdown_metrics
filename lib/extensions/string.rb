8198015d392c0a8316f946b19edfaee26175ff32
Paweł Dąbrowski <dziamber@gmail.com>
2019-02-23 13:03:56 +0100

    Add each_character_with_index String extension

--- lib/extensions/string.rb
+++ lib/extensions/string.rb
@@ -6,4 +6,16 @@ class String
       yield(chr, index + from_index) if block_given?
     end
   end
+
+  def each_character_with_index
+    self.split("").each_with_index do |chr, index|
+      next if !@skip_iteration_index.nil? && @skip_iteration_index >= index
+
+      skip_iteration_index = catch :skip_iteration do
+        yield(chr, index) if block_given?
+      end
+
+      @skip_iteration_index = skip_iteration_index if skip_iteration_index.kind_of?(Integer)
+    end
+  end
 end
--- spec/lib/extensions/string_spec.rb
+++ spec/lib/extensions/string_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe String do
   describe 'each_character_with_index_from' do
-    it 'yields each string character starting from the given index' do
+    it 'yields each character of the given string starting from the given index' do
       result = []
 
       'Miracle'.each_character_with_index_from(2) do |chr, i|
@@ -10,6 +10,29 @@ describe String do
       end
 
       expect(result).to eq([['r', 2], ['a', 3], ['c', 4], ['l', 5], ['e', 6]])
+    end
+  end
+
+  describe 'each_character_with_index' do
+    it 'yields each character of the given string' do
+      result = []
+
+      'Miracle'.each_character_with_index do |chr, i|
+        result << [chr, i]
+      end
+
+      expect(result).to eq([['M', 0], ['i', 1], ['r', 2], ['a', 3], ['c', 4], ['l', 5], ['e', 6]])
+    end
+
+    it 'yields and skip given number of interations' do
+      result = []
+
+      'Miracle'.each_character_with_index do |chr, i|
+        result << [chr, i]
+        throw :skip_iteration, 4 if i == 1
+      end
+
+      expect(result).to eq([['M', 0], ['i', 1], ['l', 5], ['e', 6]])
     end
   end
 end
