76ce0c3c9779e40d578239347b6a29ff0dc51427
Paweł Dąbrowski <dziamber@gmail.com>
2019-02-23 14:06:33 +0100

    Update h3 element naming

--- lib/markdown_metrics/elements/inline/h3.rb
+++ lib/markdown_metrics/elements/inline/h3.rb
@@ -4,8 +4,8 @@ module MarkdownMetrics
   module Elements
     module Inline
       class H3
-        def self.match(line, next_line)
-          line.match(/^\#{3} .*/)
+        def self.match_element(line, next_line)
+          line.to_s.match(/^\#{3} .*/)
         end
 
         def self.element_name
