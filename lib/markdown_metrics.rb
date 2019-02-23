3755c8a67b09d3184e207ff762c100e30de517f7
Paweł Dąbrowski <dziamber@gmail.com>
2019-02-23 14:46:41 +0100

    Load base element class

--- lib/markdown_metrics.rb
+++ lib/markdown_metrics.rb
@@ -3,6 +3,8 @@
 require 'extensions/string'
 require 'extensions/array'
 
+require 'markdown_metrics/elements/base'
+
 require 'markdown_metrics/elements/inline/h1'
 require 'markdown_metrics/elements/inline/h2'
 require 'markdown_metrics/elements/inline/h3'
@@ -13,6 +15,7 @@ require 'markdown_metrics/elements/inline/image'
 require 'markdown_metrics/elements/inline/pre'
 require 'markdown_metrics/elements/inline/empty_line'
 require 'markdown_metrics/elements/inline/paragraph'
+
 require 'markdown_metrics/elements/block/table'
 require 'markdown_metrics/elements/block/pre'
 require 'markdown_metrics/elements/block/quote'
@@ -20,6 +23,7 @@ require 'markdown_metrics/elements/block/list'
 
 require 'markdown_metrics/file_lines'
 require 'markdown_metrics/line_top_element'
+require 'markdown_metrics/top_elements_parser'
 
 
 module MarkdownMetrics
