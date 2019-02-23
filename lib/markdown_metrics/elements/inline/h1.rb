429a1f2c19cc60cc1496d9ef1c38ce85731ff76f
Paweł Dąbrowski <dziamber@gmail.com>
2019-02-23 13:57:07 +0100

    Refactor matching of h1 element

--- lib/markdown_metrics/elements/inline/h1.rb
+++ lib/markdown_metrics/elements/inline/h1.rb
@@ -4,8 +4,8 @@ module MarkdownMetrics
   module Elements
     module Inline
       class H1
-        def self.matching_rule
-          /^\#{1} .*/
+        def self.match(line, next_line)
+          line.match(/^\#{1} .*/)
         end
 
         def self.element_name
